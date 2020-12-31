f62048e5509760206f8fc63d9f911267f92d2c33
Pawel Ginalski <contact@gbyte.dev>
2020-12-31 18:32:49 +0100

    Add ability to unlock during boot

--- README.md
+++ README.md
@@ -1,10 +1,42 @@
 # zfs-multi-mount
-Mounts several datasets with only one passphrase prompt. For people who feel very confident about that one passphrase.
+Mounts several ZFS datasets while asking for encryption passphrase as rarely as possible. If the same encryption passphrase is used on several datasets, it will ask once. For people who feel very confident about that one passphrase.
 
 I generally do not endorse reusing passwords, but there are edge cases, like splitting up a pool into datasets for the sake of granular control over some of its properties while still needing the convenience of a single passphrase.
 
+This script can be used in a systemd service unlock encrypted datasets during boot. Practical if using several datasets with the same passphrase.
+
 ## Usage
+### Load keys of all datasets and mount them
+`zfs-multi-mount.sh`
+
+### Load keys for specific datasets and mount them
 `zfs-multi-mount.sh pool/dataset1 pool/dataset2 pool/dataset3`
+
+### Load keys without mounting the datasets
+`zfs-multi-mount.sh --no-mount`
+
+### Use within systemd context (in a systemd service)
+`zfs-multi-mount.sh --systemd`
+
+#### Example of a systemd service file using this script to unlock ZFS datasets
+/etc/systemd/system/zfs-load-key.service
+```
+[Unit]
+Description=Import keys for all datasets
+DefaultDependencies=no
+Before=zfs-mount.service
+Before=systemd-user-sessions.service
+After=zfs-import.target
+
+[Service]
+Type=oneshot
+RemainAfterExit=yes
+
+ExecStart=zfs-multi-mount.sh --systemd -no-mount
+
+[Install]
+WantedBy=zfs-mount.service
+```
 
 ## Credit
 Created and maintained by Pawel Ginalski (https://gbyte.dev).
--- zfs-multi-mount.sh
+++ zfs-multi-mount.sh
@@ -1,40 +1,58 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 PATH=/usr/bin:/sbin:/bin
 
 help() {
-    echo "Usage: $(basename "$0") DATASET [DATASET]..."
-    echo "Usage: $(basename "$0") pool/dataset1 pool/dataset2"
+    echo "Usage: $(basename "$0") [OPTION]... [SOURCE_POOL/DATASET]..."
     echo
-    echo " -h, --help                     show this help"
+    echo " -s, --systemd        use when within systemd context"
+    echo " -n, --no-mount       only load keys, do not mount datasets"
+    echo " -h, --help           show this help"
     exit 0
 }
 
 for arg in "$@"; do
   case $arg in
+  -s | --systemd)
+    systemd=1
+    shift
+    ;;
+  -n | --no-mount)
+    no_mount=1
+    shift
+    ;;
   -h | --help) help ;;
   -?*)
-    echo "Unknown option: $1"
-    exit 1
-    ;;
+    die "Invalid option '$1' Try '$(basename "$0") --help' for more information." ;;
   esac
 done
 
-[[ $# == 0 ]] && echo "ERROR: Datasets are needed as arguments." && exit 1
+datasets=("$@")
+[ ${#datasets[@]} -eq 0 ] && datasets=($(zfs list -H -o name))
 
-for dataset in "$@"; do
-  if ! zfs list -H -o name | grep -qx "$dataset"; then
-    echo "ERROR: Dataset '$dataset' does not exist."
-    continue
+function ask_password {
+  if [ -v systemd ]; then
+    key=$(systemd-ask-password "Enter $dataset passphrase:" --no-tty) # While booting.
+  else
+    read -srp "Enter $dataset passphrase: " key ; echo # Other places.
   fi
+}
 
-  if [[ $(zfs get keystatus "$dataset" -H -o value) == "unavailable" ]]; then
-    [ ! -v key ] && read -srp "Enter passphrase: " key ; echo
-    ! echo "$key" | zfs load-key "$dataset" && echo "ERROR: Incorrect key provided." && exit 1
+function load_key {
+  ! zfs list -H -o name | grep -qx "$dataset" && echo "ERROR: Dataset '$dataset' does not exist." && return 1
+  [[ ! $(zfs get keystatus "$1" -H -o value) == "unavailable" ]] && return 0
+  [ ! -v key ] && ask_password
+  if ! echo "$key" | zfs load-key "$1"; then
+    unset key
+    load_key $1
   fi
+  return 0
+}
 
+for dataset in "${datasets[@]}"; do
+  ! load_key $dataset && exit 1
+  
   # Mounting as non-root user on Linux is not possible,
   # see https://github.com/openzfs/zfs/issues/10648.
-  sudo zfs mount "$dataset" && echo "Dataset '$dataset' has been mounted."
+  [ ! -v no_mount ] && sudo zfs mount "$dataset" && echo "Dataset '$dataset' has been mounted."
 done
-unset key
